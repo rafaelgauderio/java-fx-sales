e7c0c0fa79c196a0389e40a6ce0a36382515c8bf
Rafael De Luca <deluca1712@gmail.com>
2020-02-08 18:05:00 -0300

    Saving new Seller

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,39 +1,29 @@
 package gui;
 
-import javafx.scene.control.TextField;
-import model.entities.Seller;
-import model.exceptions.ValidationException;
-import model.services.SellerService;
-
 import java.net.URL;
-
-import gui.listeners.DataChangeListener;
-import gui.util.Alerts;
-import gui.util.Constraints;
-import gui.util.Utils;
-
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
 
 import db.DbException;
-<<<<<<< HEAD
 import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-=======
->>>>>>> parent of 4f48bd5... DatePicker
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-<<<<<<< HEAD
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
@@ -46,9 +36,6 @@ import model.entities.Seller;
 import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 import model.services.SellerService;
-=======
-import javafx.scene.control.Label;
->>>>>>> parent of 4f48bd5... DatePicker
 
 public class SellerFormController implements Initializable {
 
@@ -65,7 +52,6 @@ public class SellerFormController implements Initializable {
 
 	@FXML
 	private TextField txtName;
-<<<<<<< HEAD
 
 	@FXML
 	private TextField txtEmail;
@@ -78,13 +64,10 @@ public class SellerFormController implements Initializable {
 
 	@FXML
 	private ComboBox<Department> comboBoxDepartment;
-=======
->>>>>>> parent of 4f48bd5... DatePicker
 
 	@FXML
 	private Label labelErrorName;
 
-<<<<<<< HEAD
 	@FXML
 	private Label labelErrorEmail;
 
@@ -94,8 +77,6 @@ public class SellerFormController implements Initializable {
 	@FXML
 	private Label labelErrorBaseSalary;
 
-=======
->>>>>>> parent of 4f48bd5... DatePicker
 	@FXML
 	private Button btSave;
 
@@ -153,15 +134,11 @@ public class SellerFormController implements Initializable {
 	private void initializeNodes() {
 
 		Constraints.setTextFieldInteger(txtId);
-<<<<<<< HEAD
 		Constraints.setTextFieldMaxLength(txtName, 50);
 		Constraints.setTextFieldDouble(txtBaseSalary);
 		Constraints.setTextFieldMaxLength(txtEmail, 50);
 		Utils.formatDatePicker(dpBirthDate, "dd/MM/yyyy");
 		initializeComboBoxDepartment();
-=======
-		Constraints.setTextFieldMaxLength(txtName, 20);
->>>>>>> parent of 4f48bd5... DatePicker
 
 	}
 
@@ -183,6 +160,25 @@ public class SellerFormController implements Initializable {
 
 		obj.setName(txtName.getText());
 
+		if (txtEmail.getText() == null || txtEmail.getText().trim().equals("")) {
+			exception.addError("email", "Field can't be empty");
+		}
+		obj.setEmail(txtEmail.getText());
+
+		if (dpBirthDate.getValue() == null) {
+			exception.addError("birthDate", "Field can't be empty");
+		} else {
+			Instant instant = Instant.from(dpBirthDate.getValue().atStartOfDay(ZoneId.systemDefault()));
+			obj.setBirthDate(Date.from(instant));
+		}
+
+		if (txtBaseSalary.getText() == null || txtBaseSalary.getText().trim().equals("")) {
+			exception.addError("baseSalary", "Field can't be empty");
+		}
+		obj.setBaseSalary(Utils.tryParseToDouble(txtBaseSalary.getText()));
+
+		obj.setDepartment(comboBoxDepartment.getValue());
+
 		if (exception.getErrors().size() > 0) {
 			throw exception;
 		}
@@ -196,7 +192,6 @@ public class SellerFormController implements Initializable {
 		}
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
-<<<<<<< HEAD
 		txtEmail.setText(entity.getEmail());
 		Locale.setDefault(Locale.US);
 		txtBaseSalary.setText(String.format("%.2f", entity.getBaseSalary()));
@@ -219,8 +214,6 @@ public class SellerFormController implements Initializable {
 		obsList = FXCollections.observableArrayList(list);
 		comboBoxDepartment.setItems(obsList);
 
-=======
->>>>>>> parent of 4f48bd5... DatePicker
 	}
 
 	private void setErrorMessages(Map<String, String> errors) {
@@ -229,6 +222,11 @@ public class SellerFormController implements Initializable {
 		if (fields.contains("name")) {
 			labelErrorName.setText(errors.get("name"));
 		}
+		labelErrorName.setText((fields.contains("name") ? errors.get("name") : ""));
+		labelErrorEmail.setText((fields.contains("email") ? errors.get("email") : ""));
+		labelErrorBirthDate.setText((fields.contains("birthDate") ? errors.get("birthDate") : ""));
+		labelErrorBaseSalary.setText((fields.contains("baseSalary") ? errors.get("baseSalary") : ""));
+
 	}
 
 	private void initializeComboBoxDepartment() {
